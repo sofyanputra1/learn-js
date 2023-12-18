584365502115b51fc75715875ce410961d83174d
sofyanputra1 <putrasofyanananta@gmail.com>
2023-12-18 10:29:42 +0700

    feat: conditional

---
+++ conditional/if-else.js
@@ -0,0 +1,10 @@
+const value = 10;
+let answer;
+
+if(value > 0) {
+    answer = "Ini adalah bilangan gede"
+}else if(value < 0) {
+    answer = "Ini adalah bilangan kecil"
+}else{
+    answer = "Bukan bilangan"
+}
