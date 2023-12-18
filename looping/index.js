bd729104b71ffc897a89ba632dc1d9542e91be03
sofyanputra1 <putrasofyanananta@gmail.com>
2023-12-18 13:57:28 +0700

    first looping

---
+++ looping/index.js
@@ -0,0 +1,11 @@
+// for ( let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+// const buah = ["PELEM","KATES","DUREN","ANGGUR"]
+// for (let i = 0; i < buah.length; i++){
+//     console.log("Buah : " + buah[i])
+// }
+const panganan = ["PIZZA","NASI KUCING","MIESOO","KAKAP GOYENG"]
+for (let i = 0; i < panganan.length; i++){
+    console.log("Saya suka makan : " + panganan[i])
+}
